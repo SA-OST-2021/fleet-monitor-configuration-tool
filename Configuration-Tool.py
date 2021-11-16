2262a2a528776a14c0e8980317a555e18f27cc9e
Florian Baumgartner <florian.baumgartner@ost.ch>
2021-11-16 18:55:48 +0100

    Updated GUI

--- Configuration-Tool.py
+++ Configuration-Tool.py
@@ -6,35 +6,54 @@ Created on Mon Nov 15 11:49:13 2021
 """
 
 import sys
+from FileHandler import FileHandler
 from PyQt5 import QtCore, QtWidgets
-from PyQt5.QtWidgets import QMainWindow, QLabel, QGridLayout, QWidget
-from PyQt5.QtWidgets import QFileDialog
-from PyQt5.QtCore import QSize  
+from PyQt5.QtCore import QSize
+from PyQt5.QtGui import QKeySequence
+from PyQt5.QtWidgets import (
+    QApplication,
+    QWidget,
+    QMainWindow,
+    QLabel,
+    QVBoxLayout,
+    QFileDialog,
+    QPushButton,
+    QShortcut,
+    QGroupBox,
+    QStatusBar,
+    QScrollArea
+)
 
 
 class App(QMainWindow):
 
     def __init__(self):
         super().__init__()
+        self.handler = FileHandler
+        self.fileName = None
+        self.fileData = None
+        
         self.initUI()
         self.setWindowState(self.windowState() & ~QtCore.Qt.WindowMinimized | QtCore.Qt.WindowActive)
-        self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
+        # self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
         self.activateWindow()
 
     def initUI(self):
-        self.setMinimumSize(QSize(750, 1000))    
-        self.setWindowTitle("Hello world") 
+        self.resize(QSize(650, 900))
+        self.setMinimumSize(QSize(200, 200))
+        self.setWindowTitle("Fleet-Monitor Configuator") 
+        self.setAcceptDrops(True)
         
-        centralWidget = QWidget(self)          
-        self.setCentralWidget(centralWidget)   
+        # centralWidget = QWidget(self)          
+        # self.setCentralWidget(centralWidget)   
  
-        gridLayout = QGridLayout(self)     
-        centralWidget.setLayout(gridLayout)  
+        # gridLayout = QGridLayout(self)     
+        # centralWidget.setLayout(gridLayout)  
  
-        title = QLabel("Fleet-Monitor Configuator", self) 
-        title.setAlignment(QtCore.Qt.AlignCenter)
-        gridLayout.addWidget(title, 0, 0)
-        
+        # title = QLabel("Fleet-Monitor Configuator", self) 
+        # title.setAlignment(QtCore.Qt.AlignCenter)
+        # gridLayout.addWidget(title, 0, 0)
+
         menu = self.menuBar().addMenu('File')
         action = menu.addAction('Open')
         action.triggered.connect(self.openFile)   
@@ -43,23 +62,98 @@ class App(QMainWindow):
         action = menu.addAction('Save as')
         action.triggered.connect(self.saveFileAs)
         
+        self.shortcut = QShortcut(QKeySequence("Ctrl+O"), self)
+        self.shortcut.activated.connect(self.openFile)
+        self.shortcut = QShortcut(QKeySequence("Ctrl+S"), self)
+        self.shortcut.activated.connect(self.saveFile)
+        self.shortcut = QShortcut(QKeySequence("Ctrl+Shift+S"), self)
+        self.shortcut.activated.connect(self.saveFileAs)
         
-    def openFile(self):
-        fileName, _ = QFileDialog.getOpenFileName(self, "Open configuration file", "", "JSON Files (*.json);;All Files (*)")
-        if fileName:
-            print(fileName)
+        self.statusbar = QStatusBar()
+        self.statusbar.setSizeGripEnabled(False)
+        self.setStatusBar(self.statusbar)
+           
+        layout = QVBoxLayout()
+        layout.addWidget(self.createGlobalSettings(), 2)
+        layout.addWidget(self.createFrameSettings(), 10)        
+
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+
+
+    def createGlobalSettings(self):
+        groupbox = QGroupBox('Global Settings')
+
+        vbox = QVBoxLayout()
+        groupbox.setLayout(vbox)
+
+        return groupbox
+    
+    def createFrameSettings(self):
+        groupbox = QGroupBox('Frame Settings')
+        vbox = QVBoxLayout()
+        
 
         
+        scroll = QtWidgets.QScrollArea()
+        scroll.setWidgetResizable(True)
+        
+        # labellist = []
+        # combolist = []
+        # for i in range(10):
+        #     labellist.append(QtWidgets.QLabel('mylabel'))
+        #     combolist.append(QtWidgets.QComboBox())
+        #     # myform.addRow(labellist[i],combolist[i])
+        #     scroll.setWidget(labellist[i],combolist[i])
+        
+        labellist = []
+        for i in range(10):
+            labellist.append(QtWidgets.QLabel('mylabel'))
+            scroll.setWidget(labellist[i])
+        
+        
+        
+        layout = QtWidgets.QVBoxLayout(self)
+        layout.addWidget(scroll)
+        
+        groupbox.setLayout(QVBoxLayout())
+        groupbox.layout().addWidget(scroll)
+        vbox.addWidget(groupbox)
+        groupbox.setLayout(vbox)
+
+        return groupbox
+
+    def dragEnterEvent(self, event):
+        if event.mimeData().hasUrls():
+            event.accept()
+        else:
+            event.ignore()
+
+    def dropEvent(self, event):
+        self.fileName = event.mimeData().urls()[0].toLocalFile()
+        self.fileData = self.handler.loadFile(self, self.fileName)
+        self.statusBar().showMessage("Open File: " + self.fileName)
+
+    def openFile(self):
+        self.fileName, _ = QFileDialog.getOpenFileName(self, "Open configuration file", "", "JSON Files (*.json);;All Files (*)")
+        if self.fileName:
+            self.fileData = self.handler.loadFile(self, self.fileName)
+            self.statusBar().showMessage("Open File: " + self.fileName)
+ 
     def saveFile(self):
-        print("Save File...")
+        if self.fileName:
+            self.handler.saveFile(self, self.fileName, self.fileData)
+            self.statusBar().showMessage("Save File: " + self.fileName)
         
     def saveFileAs(self):
-        prevName = "test.json"
-        fileName, _ = QFileDialog.getSaveFileName(self, "Save configuration file", prevName, "JSON Files (*.json);;All Files (*)")
-        if fileName:
-            print(fileName)
+        self.fileName, _ = QFileDialog.getSaveFileName(self, "Save configuration file", self.fileName, "JSON Files (*.json);;All Files (*)")
+        if self.fileName:
+            self.handler.saveFile(self, self.fileName, self.fileData)
+            self.statusBar().showMessage("Save File as: " + self.fileName)
 
- 
+
+
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     app.setQuitOnLastWindowClosed(False)
--- FileHandler.py
+++ FileHandler.py
@@ -6,31 +6,41 @@ Created on Tue Nov 16 11:14:34 2021
 """
 
 import json
+# from pathlib import path
 
 
 class FileHandler():
     def __init__(self):
         pass
+    
+    def loadFile(self, name):
+        with open(name) as f:
+            return json.load(f)
+
+    def saveFile(self, name, data):
+        with open(name, 'w') as f:
+            json.dump(data, f)
+
 
 
 if __name__ == "__main__":
-    data = {}
-    data['people'] = []
-    data['people'].append({
-        'name': 'Scott',
-        'website': 'stackabuse.com',
-        'from': 'Nebraska'
-    })
-    data['people'].append({
-        'name': 'Larry',
-        'website': 'google.com',
-        'from': 'Michigan'
-    })
-    data['people'].append({
-        'name': 'Tim',
-        'website': 'apple.com',
-        'from': 'Alabama'
-    })
     
-    with open('demo.json', 'w') as outfile:
-        json.dump(data, outfile)
+    initial = {
+        'FEE9': 'Fuel Consumption: LFC',
+        'FEFC': 'Dash Display 1: DD1',
+        'F004': 'Electronic Engine Controller #1: EEC1',
+        'FEE5': 'Engine Hours, Revolutions: HOURS',
+        'FEEC': 'Vehicle Identification: VI',
+        'FDD1': 'FMS-standard Interface Identity / Capabilities: FMS',
+        'FEC1': 'High Resolution Vehicle Distance: VDHR',
+    }
+    
+    
+    data = {}
+    data['frames'] = []
+    for i in initial:
+        data['frames'].append({'pgn': i, 'name': initial[i], 'filter': 'never'})
+    
+    
+    with open('demo.json', 'w') as f:
+        json.dump(data, f)
