06fb1572fb13df9f0f11a624bfb8f7074b604ebc
Florian Baumgartner <florian.b-h@bluewin.ch>
2021-11-19 18:39:59 +0100

    Updated QTableView

--- untitled2.py
+++ untitled2.py
@@ -20,6 +20,12 @@ from PyQt5.QtWidgets import (
     QFormLayout,
 )
 
+import PyQt5
+from PyQt5 import QtCore, QtGui, uic, QtWidgets
+from PyQt5.QtWidgets import QWidget
+
+
+
 class Window(QtWidgets.QMainWindow):
 
     def __init__(self):
@@ -39,6 +45,13 @@ class Window(QtWidgets.QMainWindow):
         self.tableWidget.setHorizontalHeaderLabels(["Active", "PGN", "Name", "Filter", "Interval [ms]"])
         self.tableWidget.verticalHeader().hide()
         
+        header = self.tableWidget.horizontalHeader()  
+        header.setSectionResizeMode(0, QtWidgets.QHeaderView.ResizeToContents)
+        header.setSectionResizeMode(1, QtWidgets.QHeaderView.ResizeToContents)
+        header.setSectionResizeMode(2, QtWidgets.QHeaderView.Stretch)
+        header.setSectionResizeMode(3, QtWidgets.QHeaderView.ResizeToContents)
+        header.setSectionResizeMode(4, QtWidgets.QHeaderView.ResizeToContents)
+        
         # self.tableWidget.setEditTriggers(QtWidgets.QTableWidget.NoEditTriggers)
         # self.tableWidget.setFocusPolicy(QtCore.NoFocus)
 
@@ -54,10 +67,12 @@ class Window(QtWidgets.QMainWindow):
             cell = QtWidgets.QTableWidgetItem()
             cell.setFlags(QtCore.Qt.ItemIsEnabled)
             self.active.append(QtWidgets.QCheckBox())
-            w = - self.active[-1].sizeHint().width() // 2
-            w = 20
-            self.active[-1].setStyleSheet(f"margin:99%;")
-            self.tableWidget.setCellWidget(row, 0, self.active[-1])
+            checkBoxWidget = QWidget()
+            layoutCheckBox = QHBoxLayout(checkBoxWidget)
+            layoutCheckBox.addWidget(self.active[-1])
+            layoutCheckBox.setAlignment(QtCore.Qt.AlignCenter)
+            layoutCheckBox.setContentsMargins(0,0,0,0)
+            self.tableWidget.setCellWidget(row, 0, checkBoxWidget)
             self.tableWidget.setItem(row, 0, cell)
             
             cell = QtWidgets.QTableWidgetItem(f"{row}")
@@ -70,9 +85,13 @@ class Window(QtWidgets.QMainWindow):
             self.tableWidget.setItem(row, 2, cell)
             self.tableWidget.viewport().setFocusPolicy(QtCore.Qt.NoFocus)
             
+            
+            
             cell = QtWidgets.QTableWidgetItem()
             cell.setFlags(QtCore.Qt.ItemIsEnabled)
             self.filters.append(QtWidgets.QComboBox())
+            filterBoxTypes = {"never": "Never", "change": "On Change", "interval": "Max. Interval"}
+            self.filters[-1].addItems(list(filterBoxTypes.values()))
             self.tableWidget.setCellWidget(row, 3, self.filters[-1])
             self.tableWidget.setItem(row, 3, cell)
             
@@ -83,14 +102,11 @@ class Window(QtWidgets.QMainWindow):
             self.tableWidget.viewport().setFocusPolicy(QtCore.Qt.NoFocus)
             
             
-            
         
 
 
-
-def run():
-    app = QtWidgets.QApplication(sys.argv)      
-    w = Window()
-    sys.exit(app.exec_())      
-
-run()
+app = QtWidgets.QApplication(sys.argv)  
+app.setQuitOnLastWindowClosed(False)
+app.lastWindowClosed.connect(QtWidgets.QApplication.quit)    
+w = Window()
+sys.exit(app.exec_())      
